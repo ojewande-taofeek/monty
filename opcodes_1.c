94cc50fee5558357ee9a01dbf0ef1728017b68fd
Patriziano <onahpatrick75@gmail.com>
2023-08-24 15:17:13 -0700

    opcodes implemented

---
+++ opcodes_1.c
@@ -0,0 +1,134 @@
+#include "monty.h"
+
+/*stack_t *stack = NULL;*/
+
+/**
+* push - A stack operation that adds an element to the stack
+* @stack: pointer to the stack
+* @line_number: value od the new node
+* Return: Nothing
+*/
+
+void push(stack_t **stack, unsigned int line_number)
+{
+	int num;
+	char *str_val;
+	stack_t *new_node;
+
+	str_val = strtok(NULL, " \t\n");
+	if (str_val == NULL)
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if (!is_digit(str_val))
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	num = atoi(str_val);
+	if (num == 0 && strcmp(str_val, "0") != 0)
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	new_node = malloc(sizeof(stack_t));
+
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new_node->n = num;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+	if (*stack != NULL)
+	{
+		(*stack)->prev = new_node;
+	}
+	*stack = new_node;
+}
+
+/**
+* pall - prints all the values on the stack
+* @stack: pointer to the stack
+* @line_number: the line number
+* Return: Nothing
+*/
+
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current;
+
+	(void)line_number;
+	current = *stack;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
+
+/**
+* pint - prints the top value of the stack
+* @stack: pointer to the stacker
+* @line_number: line number
+* Return: Nothing
+*/
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
+
+/**
+* pop - remove the top value of the stack
+* @stack: double pointer to the stack
+* @line_number: the line number
+* Return: Nothing
+*/
+
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	*stack = (*stack)->next;
+	if (*stack != NULL)
+	{
+		(*stack)->prev = NULL;
+	}
+	free(temp);
+}
+
+/**
+* swap - swap the top two elements on the stack
+* @stack: double pointer to the stack
+* @line_number: The line number
+* Return: Nothing
+*/
+
+void swap(stack_t **stack, unsigned int line_number)
+{
+	int temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
+}
